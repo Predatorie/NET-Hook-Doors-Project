636e35772a68c18dd2d5f74bd89352063ed72dac
Mick George <predatorie@live.com>
2017-11-03 19:27:25 -0400

    Added header to file

--- MDFDoor/Services/IDefaultsManager.cs
+++ MDFDoor/Services/IDefaultsManager.cs
@@ -1,4 +1,8 @@
-﻿namespace MDFDoors.Services
+﻿// <copyright file="IDefaultsManager.cs" company="Mick George @Osoy">
+// Copyright (c) Mick George @Osoy. All rights reserved.
+// </copyright>
+
+namespace MDFDoors.Services
 {
     public interface IDefaultsManager
     {
